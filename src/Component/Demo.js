9146786359d8b8fdb6de3819cdf380f7d04dc7dc
amypo-reactapp <reactapp@amypo.in>
2024-11-20 06:52:23 +0000

    User

---
+++ src/Component/Demo.js
@@ -0,0 +1,9 @@
+// import react from "react";
+
+const Demo =() =>{
+    return(
+        <h1>welcome  </h1>
+    )
+}
+
+export default Demo;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Test from './test';
 import reportWebVitals from './reportWebVitals';
-
+import Demo from "./Component/Demo"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Test />
+    <Demo />
+    {/* <h1>test</h1> */}
   </React.StrictMode>
 );
 
